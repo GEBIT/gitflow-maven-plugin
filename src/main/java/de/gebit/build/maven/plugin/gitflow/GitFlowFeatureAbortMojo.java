76b0ef93dc3dfc6841d70521c2ff1071b4d24f22
Erwin Tratar <erwin.tratar@gebit.de>
2016-10-06 17:11:24 +0200

    Added flow:release-abort and flow:feature-abort

---
+++ src/main/java/de/gebit/build/maven/plugin/gitflow/GitFlowFeatureAbortMojo.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2014-2016 Aleksandr Mashchenko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gebit.build.maven.plugin.gitflow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.codehaus.plexus.components.interactivity.PrompterException;
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.util.cli.CommandLineException;
+
+/**
+ * The git flow feature abort mojo.
+ * 
+ * @author Erwin Tratar
+ * @since 1.3.1
+ */
+@Mojo(name = "feature-abort", aggregator = true)
+public class GitFlowFeatureAbortMojo extends AbstractGitFlowMojo {
+
+    /** {@inheritDoc} */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            // check if rebase in process
+            String featureBranchName = gitRebaseBranchInProcess(); 
+            if (featureBranchName != null) {
+                throw new MojoFailureException("A rebase of the feature branch is in process. Cannot abort now.");
+            }
+            
+            // check uncommitted changes
+            checkUncommittedChanges();
+
+            // git for-each-ref --format='%(refname:short)' refs/heads/feature/*
+            final String featureBranches = gitFindBranches(
+                    gitFlowConfig.getFeatureBranchPrefix(), false);
+
+            if (StringUtils.isBlank(featureBranches)) {
+                throw new MojoFailureException("There are no feature branches.");
+            }
+
+            // fetch and check remote
+            if (fetchRemote) {
+                gitFetchRemoteAndCompare(gitFlowConfig.getDevelopmentBranch());
+            }
+
+            final String[] branches = featureBranches.split("\\r?\\n");
+
+            List<String> numberedList = new ArrayList<String>();
+            StringBuilder str = new StringBuilder("Feature branches:")
+                    .append(LS);
+            for (int i = 0; i < branches.length; i++) {
+                str.append((i + 1) + ". " + branches[i] + LS);
+                numberedList.add(String.valueOf(i + 1));
+            }
+            str.append("Choose feature branch to abort");
+
+            String featureNumber = null;
+            try {
+                while (StringUtils.isBlank(featureNumber)) {
+                    featureNumber = prompter.prompt(str.toString(),
+                            numberedList);
+                }
+            } catch (PrompterException e) {
+                getLog().error(e);
+            }
+
+            if (featureNumber != null) {
+                int num = Integer.parseInt(featureNumber);
+                featureBranchName = branches[num - 1];
+            }
+
+            if (StringUtils.isBlank(featureBranchName)) {
+                throw new MojoFailureException(
+                        "Feature branch name to abort is blank.");
+            }
+
+            // git checkout develop
+            gitCheckout(gitFlowConfig.getDevelopmentBranch());
+
+            // git branch -D feature/...
+            gitBranchDeleteForce(featureBranchName);
+        } catch (CommandLineException e) {
+            getLog().error(e);
+        }
+    }
+}
---
+++ src/main/java/de/gebit/build/maven/plugin/gitflow/GitFlowReleaseAbortMojo.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2014-2016 Aleksandr Mashchenko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gebit.build.maven.plugin.gitflow;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.util.cli.CommandLineException;
+
+/**
+ * The git flow release abort mojo.
+ * 
+ * @author Erwin Tratar
+ * @since 1.3.1
+ */
+@Mojo(name = "release-abort", aggregator = true)
+public class GitFlowReleaseAbortMojo extends AbstractGitFlowMojo {
+
+    /** {@inheritDoc} */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            // check uncommitted changes
+            checkUncommittedChanges();
+
+            // git for-each-ref --format='%(refname:short)' refs/heads/release/*
+            final String releaseBranch = gitFindBranches(
+                    gitFlowConfig.getReleaseBranchPrefix(), false).trim();
+
+            if (StringUtils.isBlank(releaseBranch)) {
+                throw new MojoFailureException("There is no release branch.");
+            } else if (StringUtils.countMatches(releaseBranch,
+                    gitFlowConfig.getReleaseBranchPrefix()) > 1) {
+                throw new MojoFailureException(
+                        "More than one release branch exists. Cannot abort release.");
+            }
+
+            // back to the development branch
+            gitCheckout(gitFlowConfig.getDevelopmentBranch());
+
+            // git branch -D release/...
+            gitBranchDeleteForce(releaseBranch);
+
+        } catch (CommandLineException e) {
+            getLog().error(e);
+        }
+    }
+}
